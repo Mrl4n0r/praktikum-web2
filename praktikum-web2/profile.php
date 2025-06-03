d8a6336d17a49b8331312dea0b1b229933706db8
Mrl4n0r <lanamau368@gmail.com>
2025-06-03 09:21:10 +0800

    Initial commit

---
+++ praktikum-web2/profile.php
@@ -0,0 +1,52 @@
+<div class="row">
+<div class="col-md-12">
+<div class="text-center">
+<img src="img/saya.png" alt="Dedy Rosyadi" width="160" class="rounded-circle">
+<table class="table mt-4" style="width:100%; margin:0 auto;">
+<tbody>
+<tr>
+<td align="right" width="50%"><strong>Nomor NPM :</strong></td>
+<td align="left" width="50%">2310010375</td>
+</tr>
+<tr>
+<td align="right" width="50%"><strong>Fakultas / Program Studio :</strong></td>
+<td align="left" width="50%">Teknologi Informasi / Teknik Informatika</td>
+</tr>
+<tr>
+<td align="right" width="50%"><strong>Nama Lengkap :</strong></td>
+<td align="left" width="50%">Muhammad Maulana</td>
+</tr>
+<tr>
+<td align="right" width="50%"><strong>Tempat, Tanggal Lahir :</strong></td>
+<td align="left" width="50%">Binjai Pemangkih, 05 Juni 2005</td>
+</tr>
+<tr>
+<td align="right" width="50%"><strong>Telepon :</strong></td>
+<td align="left" width="50%">0877 6254 4115</td>
+</tr>
+<tr>
+<td align="right" width="50%"><strong>email :</strong></td>
+<td align="left" width="50%">mhdmaulana368@gmail.com</td>
+</tr>
+<tr>
+<td align="right" width="50%"><strong>Media Sosial :</strong></td>
+<td align="left" width="50%">
+<a href="http://facebook.com/MR.L4N0R" target="_blank" style="text-decoration:none;">
+<img src="img/medsos/facebook.svg" width="30" alt="facebook">
+</a>
+<a href="http://instagram.com/its.lan_" target="_blank" style="text-decoration:none;">
+<img src="img/medsos/instagram.svg" width="30" alt="instagram">
+</a>
+<a href="http://twitter.com/MR.L4N0R" target="_blank" style="text-decoration:none;">
+<img src="img/medsos/twitter.svg" width="30" alt="twitter">
+</a>
+<a href="https://www.linkedin.com/in/muhammad-maulana-ab84b9360/" target="_blank">
+<img src="img/medsos/linkedin.svg" width="30" alt="linkedin">
+</a>
+</td>
+</tr>
+</tbody>
+</table>
+</div>
+</div>
+</div>
